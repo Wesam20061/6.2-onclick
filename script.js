1a9e0ce458204949f22ad6c133102d9100df479f
Wesam20061 <144031435+Wesam20061@users.noreply.github.com>
2023-10-12 09:15:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function KleurEnTekstAanpasen ()
+
+{
+    document.getElementById("kop1").style.color = "green"
+    document.getElementById("kop1").innerHTML = "kleur is veranderd  ✅";
+    
+
+}
+
